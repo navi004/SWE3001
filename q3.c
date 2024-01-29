24adafa829358d24a6ba1a819ea8baf2adfe07ea
Naveen N <115283282+navi004@users.noreply.github.com>
2024-01-29 15:43:27 +0530

    Bubble sort Desc Order

--- q3.c
+++ q3.c
@@ -2,34 +2,40 @@
 
 #include <stdio.h>
 
+void swap(int* a, int* b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void bubble_sort(int arr[],int n) {
+    int swapped;
+    for(int i=0;i<n-1;i++) {
+        for(int j=0;j<n-i-1;j++) {
+            swapped = 0;
+            if(arr[j] < arr[j+1]) {
+                swap(&arr[j],&arr[j+1]);
+                swapped = 1;
+            }
+            if(swapped == 0){
+                break;
+            }
+        }
+            
+    }
+}
+
 int main() {
-	int n;
-	printf("Enter the number of elemennts : ");
-
-	scanf("%d",&n);
-	int arr[n];
-	for(int i=0;i<n;i++) {
-		scanf("%d",&arr[i]);
-	}
-	printf("The array before arrange : ");
-	for(int i =0;i<n;i++) {
-
-		printf("%d ",arr[i]);
-	}
-	
-	printf("\n");
-	for(int i =0;i<n-1;i++) {
-		for(int j=i+1;j<n;j++) {
-			if(arr[i+1] < arr[i]) {
-
-				int temp = arr[i];
-				arr[i] = arr[i+1];
-				arr[i+1] = temp;
-			}
-		}
-	}
-	printf("After Ordering\n");
-	for(int i=0;i<n;i++){
-		printf("%d\n",arr[i]);
-	}
+    int n;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++) {
+        scanf("%d",&arr[i]);
+    }
+    printf("Sorted array\n");
+    bubble_sort(arr,n);
+    for(int i=0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
 }
