480f49a303e512472ba3807f3af6a7c43316709c
Naveen N <115283282+navi004@users.noreply.github.com>
2024-02-06 12:54:53 +0530

    Q1

--- Ex3/q1.c
+++ Ex3/q1.c
@@ -2,32 +2,39 @@
 /*    Print the value of common variable before the forthest is called.The parent process modifies values of c     ommon variable which is unaware to the child process.
     Write separate functions for parent process and child process using c program*/
     
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
 
-int main(int argc, char **argv) {
-    pid_t pid = fork();
-    
-    int common_variable;
-    
-    if (pid==0) {
-        printf("This is the Child process and pid is: %d\n",getpid());
-        printf("This is the Parent process id of Child Process : %d\n",getppid());
-        common_variable = 1;
-        printf("Value of common variable in Child Process = %d\n",common_variable);
-        exit(0);
-        
-    } else if (pid > 0) {
-        printf("This is the Parent process and pid is: %d\n",getpid());
-        common_variable = 0;
-        printf("Value of common variable in Parent Process = %d\n",common_variable);
-    } else {
-        printf("Error while forking\n");
-        exit(EXIT_FAILURE);
-    }
-    return 0;
+int common_var = 0;
+
+void parentProcess() {
+    common_var++;
+    printf("Parent Process PID : %d\n",getppid());
+    printf("common variable value in Parent Process = %d\n\n", common_var);
 }
 
+void childProcess() {
+    printf("Child Process PID : %d\n",getpid());
+    printf("common variable value in Child Process = %d\n", common_var);
+}
+
+int main() {
+    pid_t pid = fork();
+    
+    printf("Initial Common Variable = %d\n",common_var);
+
+    if (pid < 0) {
+        fprintf(stderr, "Fork failed\n");
+        return 1;
+    } else if (pid == 0) {
+        // Child process
+        childProcess();
+    } else {
+        // Parent process
+        parentProcess();
+    }
+
+    return 0;
+}
     
