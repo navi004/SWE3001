8b46b5e2e8fdc0938ee7f2ad794068d630e4d87e
Naveen N <115283282+navi004@users.noreply.github.com>
2024-02-20 10:45:00 +0530

    Update q3.c

--- Ex3/q3.c
+++ Ex3/q3.c
@@ -1,2 +1,11 @@
 /*
-    Write a c program 
+write a c program to illustrate process termination. 
+    Create 5 processes among which one is the parent process and 4 child processes terminate all processes in the sequence given below : 
+    (i) The parent process should terminate last after all the child processes 
+    (ii) The fourth child process should terminate before the parent process. 
+    (iii) The first child process terminates after the third child process. 
+    (iv) The second child process should terminate before the first child process and after the third process 
+    (v) The third child process should terminate first. 
+    Print the termination in the sequence along with the ID of each process.
+    */
+
