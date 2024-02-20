8c4a463f30e7a11c7e849ce73e7282093ce60fa5
Naveen N <115283282+navi004@users.noreply.github.com>
2024-02-20 11:02:53 +0530

    Update q4.c

--- Ex3/q4.c
+++ Ex3/q4.c
@@ -1,23 +1,36 @@
 // A C program to demonstrate Zombie Process. 
 // Child becomes Zombie as parent is sleeping 
 // when child process exits. 
-#include <stdlib.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
-int main() 
-{ 
-	// Fork returns process id 
-	// in parent process 
-	pid_t child_pid = fork(); 
 
-	// Parent process 
-	if (child_pid > 0) 
-		sleep(50); 
 
-	// Child process 
-	else		
-		exit(0); 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-	return 0; 
-} 
+int main() {
+    pid_t child_pid;
 
+    // Create a child process
+    child_pid = fork();
+
+    if (child_pid == -1) {
+        // Fork failed
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (child_pid > 0) {
+        // Parent process
+        printf("Parent process (PID %d) is sleeping for 10 seconds.\n", getpid());
+        sleep(10);
+
+        // The parent process does not call wait, leaving the child in a zombie state
+        printf("Parent process exiting without waiting for the child.\n");
+    } else {
+        // Child process
+        printf("Child process (PID %d) is exiting.\n", getpid());
+        exit(EXIT_SUCCESS);
+    }
+
+    return 0;
+}
