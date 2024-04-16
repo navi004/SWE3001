53df3c0901cd71144840f9faff727b45ef2d558d
Naveen N <115283282+navi004@users.noreply.github.com>
2024-04-16 12:07:38 +0530

    Update dma.c

--- Ex8/dma.c
+++ Ex8/dma.c
@@ -1,1 +1,2 @@
-
+#write a c program to implement dynamic memory allocation technique for the given memory partition sizes 20KB, 100KB, 40KB, 200KB and 10KB 
+#for the various processes of size 90KB, 50KB, 30KB and 40KB. Display the allocation of all the processes by applying the algorithms on the screen.
